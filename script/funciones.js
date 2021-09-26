6af69fe509289402553daac0c7cff9e2b1457e68
andresfvb <barandicandres@gmail.com>
2021-09-25 22:24:33 -0500

    Inicio Heladeria

---
+++ script/funciones.js
@@ -0,0 +1,21 @@
+var ventas = 0;
+
+function ingresClientes(){
+
+}
+
+function denominacionCinco(){
+
+}
+
+function denominacionDiez(){
+
+}
+
+function denominacionVeinte(){
+
+}
+
+function regresarVueltos(){
+    
+}
